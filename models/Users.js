de925db1b359968a1832c1102e4c74d5f67b8f62
Brochb <brochbaltzer@gmail.com>
2023-10-03 21:29:15 -0600

    created models, and linked them all together

---
+++ models/Users.js
@@ -0,0 +1,34 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+
+// import our database connection from config.js
+const sequelize = require('../config/connection.js');
+
+// Initialize Users model (table) by extending off Sequelize's Model class
+class Users extends Model { }
+
+// set up fields and rules for Users model
+Users.init(
+  {
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    users_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users',
+  }
+);
+
+module.exports = Users;
+
