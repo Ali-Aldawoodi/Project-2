20dbe25e90118ca0544d73b707e6d66c41241538
Brochb <brochbaltzer@gmail.com>
2023-10-05 19:30:39 -0600

    get all reviews, and get one review, get one user functionality working!

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userRoutes = require('./user-routes');
-// const reviewRoutes = require('./review-routes');
+const reviewRoutes = require('./review-routes');
 
 router.use('/users', userRoutes);
-// router.use('/reviews', reviewRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
