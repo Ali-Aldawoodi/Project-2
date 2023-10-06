3740da93f3352b63ac25577ab454ad7b3a00b837
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-10-05 21:21:50 -0600

    pulling

--- models/index.js
+++ models/index.js
@@ -3,13 +3,13 @@ const Users = require('./Users');
 const Reviews = require('./Reviews');
 
 // Reviews belongsTo Users
-Reviews.belongsTo(Users, {
+Reviews.hasMany(Users, {
   foreignKey: 'reviews_id',
 });
 
 // Reviews belongsTo Users
 Reviews.belongsTo(Users, {
-  foreignKey: 'users_id'
+  foreignKey: 'reviews_id'
 });
 
 module.exports = {
