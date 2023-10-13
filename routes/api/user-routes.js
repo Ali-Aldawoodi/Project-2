3e83a14aac51505c15f8c0239b528997c9241c39
Brochb <brochbaltzer@gmail.com>
2023-10-12 20:16:49 -0600

    moving routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -116,32 +116,28 @@ router.post('/login', async (req, res) => {
     const userData = await Users.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
-      return;
+      return res.status(400).json({ message: 'Incorrect username or password, please try again' });
     }
 
-    const validPassword = userData.checkPassword(req.body.password);
+    const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
-      return;
+      return res.status(400).json({ message: 'Incorrect username or password, please try again' });
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.loggedIn = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
+      res.redirect('/homepage');
     });
 
   } catch (err) {
     res.status(400).json(err);
   }
 });
+
 
 router.post('/logout', (req, res) => {
   console.log('log out route');
